afa8b414b8bec097939ca7c5820583a3ae166e6b
Abdulla Akeel <abdullaakeelfloger@gmail.com>
2025-12-28 17:33:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9423937412-c9e15",
+  "appId": "1:145898874792:web:b947f07154de7142a61f9d",
+  "apiKey": "AIzaSyBqoAhOBoViAq56WHCeP7YQBtEqCtokhdw",
+  "authDomain": "studio-9423937412-c9e15.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "145898874792"
+};
